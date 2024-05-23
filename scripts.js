2f58c66311f60e64476b9cdf9d2e34dc2dd0a208
Marcel Schoombee <marcelsch01@yahoo.com>
2024-05-23 11:11:18 +0200

    log with forEach

--- scripts.js
+++ scripts.js
@@ -13,3 +13,14 @@ const products = [
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
 ]
+
+
+// Log each name
+names.forEach(name => console.log(name));
+
+// Log each province
+provinces.forEach(province => console.log(province));
+
+// Log each name with a matching province
+names.forEach((name, index) => console.log(`${name} (${provinces[index]})`));
+
